8ca59cc435429b2c01507e2899b8576df97faf7b
Rc-Cookie <rccookie@mail.de>
2023-12-04 08:23:51 +0100

    Day 4

---
+++ src/main/java/de/rccookie/aoc/aoc23/Solution4.java
@@ -0,0 +1,36 @@
+package de.rccookie.aoc.aoc23;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import de.rccookie.aoc.Solution;
+import de.rccookie.math.Mathf;
+
+public class Solution4 extends Solution {
+
+    @Override
+    public Object task1() {
+        return matchCounts().map(c -> c == 0 ? 0 : 1 << (c-1)).sum();
+    }
+
+    @Override
+    public Object task2() {
+        int[] matchCounts = matchCounts().toArray();
+        int[] counts = new int[matchCounts.length];
+        Arrays.fill(counts, 1);
+        for(int i=0; i<counts.length-1; i++)
+            for(int j=0; j<matchCounts[i]; j++)
+                counts[i+j+1] += counts[i];
+        return Mathf.sum(counts);
+    }
+
+    IntStream matchCounts() {
+        return lines.mapToInt(line -> {
+            String[] parts = line.split("\\s*[:|]\\s*");
+            Set<Integer> winning = Arrays.stream(parts[1].split("\\s+")).map(Integer::parseInt).collect(Collectors.toSet());
+            return (int) Arrays.stream(parts[2].split("\\s+")).map(Integer::parseInt).filter(winning::contains).count();
+        });
+    }
+}
