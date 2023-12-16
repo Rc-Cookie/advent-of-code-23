209a1713cae6bb888de68d7d7f8f57808a2d8753
Rc-Cookie <leomue@mail.de>
2023-12-17 00:02:05 +0100

    Use long[] instead of byte[][]

--- src/main/java/de/rccookie/aoc/aoc23/Solution16.java
+++ src/main/java/de/rccookie/aoc/aoc23/Solution16.java
@@ -5,6 +5,8 @@ import de.rccookie.math.Mathf;
 import de.rccookie.math.constInt2;
 import de.rccookie.math.int2;
 
+import static de.rccookie.aoc.aoc23.Utils.*;
+
 public class Solution16 extends Solution {
 
     private static final byte RIGHT = 0;
@@ -25,49 +27,52 @@ public class Solution16 extends Solution {
 
     @Override
     public Object task1() {
-        return findEnergized(int2.zero, RIGHT);
+        return findEnergized(int2.zero, RIGHT, charTable[0].length, charTable.length);
     }
 
     @Override
     public Object task2() {
+        int width = charTable[0].length;
+        int height = charTable.length;
+
         int max = 0;
         for(int i=0; i<charTable.length; i++) {
             max = Mathf.max(
                     max,
-                    findEnergized(new int2(0, i), RIGHT),
-                    findEnergized(new int2(charTable.length-1, i), LEFT)
+                    findEnergized(new int2(0, i), RIGHT, width, height),
+                    findEnergized(new int2(charTable.length-1, i), LEFT, width, height)
             );
         }
         for(int i=0; i<charTable[0].length; i++) {
             max = Mathf.max(
                     max,
-                    findEnergized(new int2(i, 0), DOWN),
-                    findEnergized(new int2(i, charTable[0].length-1), UP)
+                    findEnergized(new int2(i, 0), DOWN, width, height),
+                    findEnergized(new int2(i, charTable[0].length-1), UP, width, height)
             );
         }
         return max;
     }
 
-    private int findEnergized(constInt2 start, int dir) {
-        byte[][] energized = new byte[charTable.length][charTable[0].length];
-        followLight(start, dir, energized);
+    private int findEnergized(constInt2 start, int dir, int width, int height) {
+        long[] energized = new long[(width * height * 4 + 63) >> 6];
+        followLight(start, dir, energized, width, height);
         int count = 0;
-        for(int i=0; i<energized.length; i++)
-            for(int j=0; j<energized[i].length; j++)
-                if(energized[i][j] != 0)
+        for(long l : energized)
+            for(int i=0; i<64; i+=4)
+                if((l >> i & 0xF) != 0)
                     count++;
         return count;
     }
 
-    private void followLight(constInt2 start, int dir, byte[][] energized) {
+    private void followLight(constInt2 start, int dir, long[] energized, int width, int height) {
         int2 pos = start.clone();
-        while(pos.geq(constInt2.zero) && pos.y() < energized.length && pos.x() < energized[0].length && (energized[pos.y()][pos.x()] & 1 << dir) == 0) {
-            energized[pos.y()][pos.x()] |= (byte) (1 << dir);
+        while(pos.geq(constInt2.zero) && pos.x() < width && pos.y() < height && !gbi(energized, pos.x(), pos.y(), width, 4, dir)) {
+            sbi(energized, pos.x(), pos.y(), width, 4, dir);
 
             char c = charTable[pos.y()][pos.x()];
             if(c == SPLITTER_FOR_DIR[dir]) {
                 dir = NEXT_DIRS[c][dir];
-                followLight(pos, (dir+2) & 3, energized);
+                followLight(pos, (dir+2) & 3, energized, width, height);
             }
             else dir = NEXT_DIRS[c][dir];
             pos.add(DIRS[dir]);
--- src/main/java/de/rccookie/aoc/aoc23/Utils.java
+++ src/main/java/de/rccookie/aoc/aoc23/Utils.java
@@ -11,13 +11,28 @@ final class Utils {
         return (data[i >> 6] & 1L << (i & 63)) != 0;
     }
 
+    public static boolean gbi(long[] data, int x, int y, int width, int step, int off) {
+        int i = (x + width * y) * step + off;
+        return (data[i >> 6] & 1L << (i & 63)) != 0;
+    }
+
     public static void sbi(long[] data, int x, int y, int width) {
         int i = x + width * y;
         data[i >> 6] |= 1L << (i & 63);
     }
 
+    public static void sbi(long[] data, int x, int y, int width, int step, int off) {
+        int i = (x + width * y) * step + off;
+        data[i >> 6] |= 1L << (i & 63);
+    }
+
     public static void cbi(long[] data, int x, int y, int width) {
         int i = x + width * y;
+        data[i >> 6] &= ~(1L << (i & 63));
+    }
+
+    public static void cbi(long[] data, int x, int y, int width, int step, int off) {
+        int i = (x + width * y) * step + off;
         data[i >> 6] &= ~(1L << (i & 63));
     }
 }
