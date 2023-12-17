49d37b761a14a63dbfed76c355975eae19592664
Rc-Cookie <leomue@mail.de>
2023-12-17 16:36:04 +0100

    Flatten int[][] to int[]

--- src/main/java/de/rccookie/aoc/aoc23/Solution17.java
+++ src/main/java/de/rccookie/aoc/aoc23/Solution17.java
@@ -6,6 +6,7 @@ import java.util.Comparator;
 import de.rccookie.aoc.Solution;
 import de.rccookie.graph.BinaryHeap;
 import de.rccookie.graph.Heap;
+import de.rccookie.util.Console;
 
 public class Solution17 extends Solution {
 
@@ -21,39 +22,40 @@ public class Solution17 extends Solution {
 
     private int findPath(int minMove, int maxMove) {
 
-        int[][] graph = new int[1 << 17][2 * (maxMove - minMove + 1)];
+        int capacity = 2 * (maxMove - minMove + 1);
+        int[] graph = new int[(1 << 17) * capacity];
 
         for(int i=0; i<charTable.length; i++) {
             for(int j=0; j<charTable[i].length; j++) {
 
-                int node = transition(j,i,0,0);
+                int node = transition(j,i,0,0) * capacity;
                 int dist = 0;
                 int nextIndex = 0;
                 for(int k=1; k<=maxMove && k<=j; k++) {
                     dist += charTable[i][j-k] - '0';
                     if(k >= minMove)
-                        graph[node][nextIndex++] = 1 << 31 | transition(j - k, i, 1, dist);
+                        graph[node + nextIndex++] = 1 << 31 | transition(j - k, i, 1, dist);
                 }
                 dist = 0;
                 for(int k=1; k<=maxMove && j+k<charTable[0].length; k++) {
                     dist += charTable[i][j + k] - '0';
                     if(k >= minMove)
-                        graph[node][nextIndex++] = 1 << 31 | transition(j + k, i, 1, dist);
+                        graph[node + nextIndex++] = 1 << 31 | transition(j + k, i, 1, dist);
                 }
 
-                node ^= 1;
+                node += capacity;
                 dist = 0;
                 nextIndex = 0;
                 for(int k=1; k<=maxMove && k<=i; k++) {
                     dist += charTable[i-k][j] - '0';
                     if(k >= minMove)
-                        graph[node][nextIndex++] = 1 << 31 | transition(j, i - k, 0, dist);
+                        graph[node + nextIndex++] = 1 << 31 | transition(j, i - k, 0, dist);
                 }
                 dist = 0;
                 for(int k=1; k<=maxMove && i+k<charTable.length; k++) {
                     dist += charTable[i+k][j] - '0';
                     if(k >= minMove)
-                        graph[node][nextIndex++] = 1 << 31 | transition(j, i + k, 0, dist);
+                        graph[node + nextIndex++] = 1 << 31 | transition(j, i + k, 0, dist);
                 }
             }
         }
@@ -61,11 +63,11 @@ public class Solution17 extends Solution {
         int start = 0;
         int end = transition(charTable[0].length - 1, charTable.length - 1, 0, 0);
         int count = 0;
-        while(graph[start][count] != 0) count++;
-        System.arraycopy(graph[start|1], 0, graph[start], count, count);
-        graph[start|1][0] = 0;
+        while(graph[count] != 0) count++;
+        System.arraycopy(graph, capacity, graph, count, count);
+        graph[capacity] = 0;
 
-        return shortestPath(graph, start, end);
+        return shortestPath(graph, capacity, start, end);
     }
 
     private static int transition(int x, int y, int state, int dist) {
@@ -84,14 +86,13 @@ public class Solution17 extends Solution {
         return x & 0x1FFFF;
     }
 
-    private static int shortestPath(int[][] graph, int start, int end) {
+    private static int shortestPath(int[] graph, int capacity, int start, int end) {
 
-        short[] d = new short[graph.length];
+        short[] d = new short[graph.length / capacity];
         Arrays.fill(d, Short.MAX_VALUE);
         d[start] = 0;
 
         Heap<Integer> q = new BinaryHeap<>(Comparator.comparingInt(n -> d[n] + dist(n, end)));
-//        BinaryIntHeap q = new BinaryIntHeap();
         q.enqueue(start);
 
         while(!q.isEmpty()) {
@@ -100,7 +101,9 @@ public class Solution17 extends Solution {
             if(n == end || n == (end|1))
                 return nd;
 
-            for(int adj : graph[n]) {
+            for(int i=0; i<capacity; i++) {
+                int adj = graph[n * capacity + i];
+                if(adj == 0) break;
                 int dist = nd + dist(adj);
                 int m = node(adj);
                 if(dist < d[m]) {
