ec21442716847c3e30f2c00f3da77f28473cc94e
Rc-Cookie <leomue@mail.de>
2023-12-17 15:48:40 +0100

    Replace generic graph and search with arrays

--- src/main/java/de/rccookie/aoc/aoc23/Solution17.java
+++ src/main/java/de/rccookie/aoc/aoc23/Solution17.java
@@ -1,16 +1,11 @@
 package de.rccookie.aoc.aoc23;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
-import com.diogonunes.jcolor.Attribute;
 import de.rccookie.aoc.Solution;
-import de.rccookie.graph.DistancePath;
-import de.rccookie.graph.Graph;
-import de.rccookie.graph.Graphs;
-import de.rccookie.graph.HashGraph;
-import de.rccookie.math.Mathf;
-import de.rccookie.math.int3;
-import de.rccookie.util.Console;
+import de.rccookie.graph.BinaryHeap;
+import de.rccookie.graph.Heap;
 
 public class Solution17 extends Solution {
 
@@ -26,90 +21,96 @@ public class Solution17 extends Solution {
 
     private int findPath(int minMove, int maxMove) {
 
-        Graph<Integer, Integer> graph = new HashGraph<>();
+        int[][] graph = new int[1 << 17][2 * (maxMove - minMove + 1)];
 
         for(int i=0; i<charTable.length; i++) {
             for(int j=0; j<charTable[i].length; j++) {
 
-                int node = compress(j,i,0);
+                int node = transition(j,i,0,0);
                 int dist = 0;
+                int nextIndex = 0;
                 for(int k=1; k<=maxMove && k<=j; k++) {
                     dist += charTable[i][j-k] - '0';
                     if(k >= minMove)
-                        graph.connect(node, compress(j-k,i,1), dist);
+                        graph[node][nextIndex++] = 1 << 31 | transition(j - k, i, 1, dist);
                 }
                 dist = 0;
                 for(int k=1; k<=maxMove && j+k<charTable[0].length; k++) {
-                    dist += charTable[i][j+k] - '0';
+                    dist += charTable[i][j + k] - '0';
                     if(k >= minMove)
-                        graph.connect(node, compress(j+k,i,1), dist);
+                        graph[node][nextIndex++] = 1 << 31 | transition(j + k, i, 1, dist);
                 }
 
                 node ^= 1;
                 dist = 0;
+                nextIndex = 0;
                 for(int k=1; k<=maxMove && k<=i; k++) {
                     dist += charTable[i-k][j] - '0';
                     if(k >= minMove)
-                        graph.connect(node, compress(j,i-k,0), dist);
+                        graph[node][nextIndex++] = 1 << 31 | transition(j, i - k, 0, dist);
                 }
                 dist = 0;
                 for(int k=1; k<=maxMove && i+k<charTable.length; k++) {
                     dist += charTable[i+k][j] - '0';
                     if(k >= minMove)
-                        graph.connect(node, compress(j,i+k,0), dist);
+                        graph[node][nextIndex++] = 1 << 31 | transition(j, i + k, 0, dist);
                 }
             }
         }
 
         int start = 0;
-        int end = compress(charTable[0].length - 1, charTable.length - 1, 0);
-        graph.connectToAll(start, graph.adj(start|1).keySet(), (a,b) -> graph.edge(start|1, b));
-        graph.remove(start|1);
-        graph.set(end|1, end);
+        int end = transition(charTable[0].length - 1, charTable.length - 1, 0, 0);
+        int count = 0;
+        while(graph[start][count] != 0) count++;
+        System.arraycopy(graph[start|1], 0, graph[start], count, count);
+        graph[start|1][0] = 0;
 
-        DistancePath<Integer, Integer> path = Graphs.shortestPath(
-                graph,
-                start,
-                end,
-                Integer::doubleValue,
-                Solution17::dist
-        );
-//        showPath(path);
-        return Mathf.round(path.distance());
+        return shortestPath(graph, start, end);
     }
 
-    private static int compress(int x, int y, int state) {
-        return x << 9 | y << 1 | state;
+    private static int transition(int x, int y, int state, int dist) {
+        return dist << 17 | x << 9 | y << 1 | state;
     }
 
     private static int dist(int a, int b) {
-        return Math.abs((b >> 9) - (a >> 9)) + Math.abs((b >> 1 & 0xFF) - (a >> 1 & 0xFF));
+        return Math.abs((b >> 9 & 0xFF) - (a >> 9 & 0xFF)) + Math.abs((b >> 1 & 0xFF) - (a >> 1 & 0xFF));
     }
 
-    private static int3 decompress(int data) {
-        return new int3(data >> 9 & 0xFF, data >> 1 & 0xFF, data & 1);
+    private static int dist(int x) {
+        return x >> 17 & 0xFF;
     }
 
-    private void showPath(DistancePath<Integer, Integer> path) {
-        String[][] output = new String[charTable.length][charTable[0].length];
-        for(int i=0; i<output.length; i++) {
-            int _i = i;
-            Arrays.setAll(output[i], j -> charTable[_i][j] + "");
+    private static int node(int x) {
+        return x & 0x1FFFF;
+    }
+
+    private static int shortestPath(int[][] graph, int start, int end) {
+
+        short[] d = new short[graph.length];
+        Arrays.fill(d, Short.MAX_VALUE);
+        d[start] = 0;
+
+        Heap<Integer> q = new BinaryHeap<>(Comparator.comparingInt(n -> d[n] + dist(n, end)));
+//        BinaryIntHeap q = new BinaryIntHeap();
+        q.enqueue(start);
+
+        while(!q.isEmpty()) {
+            int n = q.dequeue();
+            short nd = d[n];
+            if(n == end || n == (end|1))
+                return nd;
+
+            for(int adj : graph[n]) {
+                int dist = nd + dist(adj);
+                int m = node(adj);
+                if(dist < d[m]) {
+                    d[m] = (short) dist;
+                    if(!q.updateDecreased(m))
+                        q.enqueue(m);
+                }
+            }
         }
 
-        path.forEach((aa, bb, d) -> {
-            int3 a = decompress(aa), b = decompress(bb);
-            int3 diff = b.subed(a);
-            for(int i=0; i<-diff.x(); i++)
-                output[a.y()][a.x()-i] = Console.colored("<", Attribute.BOLD(), Attribute.RED_TEXT());
-            for(int i=0; i<diff.x(); i++)
-                output[a.y()][a.x()+i] = Console.colored(">", Attribute.BOLD(), Attribute.RED_TEXT());
-            for(int i=0; i<-diff.y(); i++)
-                output[a.y()-i][a.x()] = Console.colored("^", Attribute.BOLD(), Attribute.RED_TEXT());
-            for(int i=0; i<diff.y(); i++)
-                output[a.y()+i][a.x()] = Console.colored("v", Attribute.BOLD(), Attribute.RED_TEXT());
-        });
-        for(int i=0; i<charTable.length; i++)
-            System.out.println(String.join("", output[i]));
+        return -1;
     }
 }
