e3d6d850b6154fc4fc968b7dab3008a564e89839
Rc-Cookie <leomue@mail.de>
2023-12-19 23:54:58 +0100

    Optimization

--- src/main/java/de/rccookie/aoc/aoc23/Solution18.java
+++ src/main/java/de/rccookie/aoc/aoc23/Solution18.java
@@ -12,6 +12,13 @@ public class Solution18 extends Solution {
         DIRS_X['2'] = DIRS_X['L'] = -1;
         DIRS_Y['3'] = DIRS_Y['U'] = -1;
     }
+    private static final int[] CHAR_VAL = new int[128];
+    static {
+        for(int i=0; i<10; i++)
+            CHAR_VAL['0'+i] = i;
+        for(int i=0; i<6; i++)
+            CHAR_VAL['A'+i] = CHAR_VAL['a'+i] = 10+i;
+    }
 
     @Override
     public Object task1() {
@@ -20,17 +27,25 @@ public class Solution18 extends Solution {
         int lastX = 0;
         int lastY = 0;
 
-        for(int i=0; i<linesArr.length; i++) {
-            int len = Integer.parseInt(linesArr[i], 2, linesArr[i].indexOf(' ', 3), 10);
+        int pos = 0;
+        do {
+            char c = chars[pos];
+            pos += 2;
+            int len = CHAR_VAL[chars[pos]];
+            if(chars[pos+1] != ' ')
+                len = 10 * len + CHAR_VAL[chars[++pos]];
             perimeter += len;
-            char c = linesArr[i].charAt(0);
+
             int x = lastX + DIRS_X[c] * len;
             int y = lastY + DIRS_Y[c] * len;
             area += lastX * y - lastY * x;
             lastX = x;
             lastY = y;
-        }
-        return (Math.abs(area) + perimeter) / 2 + 1;
+
+            pos += 12;
+        } while(pos < chars.length);
+
+        return ((area + perimeter) >> 1) + 1;
     }
 
     @Override
@@ -40,16 +55,22 @@ public class Solution18 extends Solution {
         int lastX = 0;
         int lastY = 0;
 
-        for(int i=0; i<linesArr.length; i++) {
-            int len = Integer.parseInt(linesArr[i], linesArr[i].indexOf('#', 5) + 1, linesArr[i].length()-2, 16);
+        int pos = 0;
+        do {
+            pos += 6;
+            if(chars[pos] == '#') pos++;
+            int len = CHAR_VAL[chars[pos]] << 16 | CHAR_VAL[chars[pos+1]] << 12 | CHAR_VAL[chars[pos+2]] << 8 | CHAR_VAL[chars[pos+3]] << 4 | CHAR_VAL[chars[pos+4]];
             perimeter += len;
-            char c = linesArr[i].charAt(linesArr[i].length() - 2);
-            int x = lastX + DIRS_X[c] * len;
-            int y = lastY + DIRS_Y[c] * len;
+
+            int x = lastX + DIRS_X[chars[pos+5]] * len;
+            int y = lastY + DIRS_Y[chars[pos+5]] * len;
             area += (long) lastX * y - (long) lastY * x;
             lastX = x;
             lastY = y;
-        }
-        return (Math.abs(area) + perimeter) / 2 + 1;
+
+            pos += 8;
+        } while(pos != chars.length);
+
+        return ((area + perimeter) >> 1) + 1;
     }
 }
